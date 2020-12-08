4b0f60617f5bcdc81c53d42f08631f411a1de4c1
Callum <calmelb@live.com>
2020-12-08 14:15:51 +1100

    Day 6 done & Prep for day 7+8

--- Day6/day6.py
+++ Day6/day6.py
@@ -19,15 +19,24 @@ for i in text:
 
 for group in plain_text:
     groupTempList = []
+    groupTempDup = {}
+    groupSize = len(group)
     for person in group:
         for item in person:
-            if item not in groupTempList:
-                groupTempList.append(item)
+            if item not in groupTempDup:
+                groupTempDup[item] = 1
+            else:
+                groupTempDup[item] = groupTempDup[item] + 1
+
+    for key in groupTempDup:
+        if groupTempDup[key] == groupSize:
+            groupTempList.append(key)
+
     groupList.append(groupTempList)
     groupListNumber.append(len(groupTempList))
+
 
 for num in groupListNumber:
     totalNum = totalNum + num
 
-print(groupList)
-print(groupListNumber)
+print(totalNum)
