ef1880d960da6e47774b55f59d4a9ad6accd38fe
Callum Bundey <callum.bundey@anu.edu.au>
2020-12-09 18:54:05 +1100

    Day 7 pt 1 done

--- Day7/day7.py
+++ Day7/day7.py
@@ -1,17 +1,90 @@
-import re
+debug = 1
 
-file = open("test.txt","r")
+if debug == 0:
+    file = open("input.txt","r")
+else:
+    file = open("test.txt","r")
+
 text = file.readlines()
-plain_text = []
-
+bagList = {}
+tmplist = {}
+fullListBag = []
 
 for i in text:
-    tmplist = {}
+
+    tmpContentsDict = {}
     words = i.split()
-    print(words)
     contents = ' '.join(words[4:]).split(", ")
-    tmplist[' '.join(words[:3])] = contents
-    plain_text.append(tmplist)
+    itemJoined = ' '.join(' '.join(words[:3]).split(" ")[:2])
+    for item in contents:
+        if item == "no other bags.":
+            tmpContentsDict["None"] = 0
+        else:
+            joined = ' '.join(item[2:].split()[:2])
+            tmpContentsDict[joined] = item[:1]
+    if "no other bags." in contents:
+        tmplist[' '.join(words[:3])] = tmpContentsDict
+    else:
+        tmplist[itemJoined] = tmpContentsDict
+    bagList = tmplist
 
 
-print(plain_text)
+def getSupportingBags(bagRequest):
+    listOfBags = []
+
+    for item in bagList:
+        for bag in bagList.get(item):
+            if bag == bagRequest:
+                listOfBags.append(item)
+                listOfBags.append(getSupportingBags(item))
+    return listOfBags
+
+def inList(item):
+    if item not in fullListBag:
+        fullListBag.append(item)
+    # else:
+    #     print("Already In!")
+
+def listOrganisation(bagrequest):
+    listBag = getSupportingBags(bagrequest)
+    for i in listBag:
+        if isinstance(i, list):
+            listExplosion(i)
+            # print(i)
+        else:
+            inList(i)
+
+def listExplosion(listIn):
+    if isinstance(listIn, list):
+        for j in listIn:
+            listExplosion(j)
+    else:
+        inList(listIn)
+
+    # for j in listIn:
+    #     if isinstance(j, list):
+    #         for i in j:
+    #             listExplosion(i)
+    #     else:
+    #         inList(j)
+
+
+
+listOrganisation("shiny gold")
+print(fullListBag)
+
+print("\nLength: " + str(len(fullListBag))+"\n")
+print(bagList)
+
+# /---Part 2---\
+
+newBags = []
+newBagCount = 0
+
+def bagContents(bagrequest):
+    # {'dark olive': '1', 'vibrant plum': '2'}
+    bagInside = bagList.get(bagrequest)
+    for item in bagrequest
+
+
+
